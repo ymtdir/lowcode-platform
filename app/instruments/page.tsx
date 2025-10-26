71891e772921e6316ff2b52d1b753a8e9ea26b2e
ymtdir <ymtdir0407@gmail.com>
2025-10-27 00:29:19 +0900

    feat: Supabase Authによる認証基盤の実装

--- app/instruments/page.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@/lib/supabase/server';
-
-export default async function Instruments() {
-  const supabase = await createClient();
-  const { data: instruments } = await supabase.from('instruments').select();
-
-  return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
-}
