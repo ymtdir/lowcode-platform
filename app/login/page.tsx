7dbd2ba6920e6bfcd472f2dd805815564527a3c1
ymtdir <ymtdir0407@gmail.com>
2025-10-27 21:00:44 +0900

    feat: ログインフォームを実装

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,66 @@
 import { login, signup } from './actions';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex min-h-screen items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>アカウントにログイン</CardTitle>
+          <CardDescription>
+            メールアドレスを入力してログインしてください
+          </CardDescription>
+          <CardAction>
+            <Button variant="link">新規登録</Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form action={login}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">メールアドレス</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="example@mail.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">パスワード</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    パスワードをお忘れですか？
+                  </a>
+                </div>
+                <Input id="password" name="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                ログイン
+              </Button>
+              <Button variant="outline" className="w-full">
+                Googleでログイン
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2"></CardFooter>
+      </Card>
+    </div>
   );
 }
